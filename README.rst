c9fbb02fdc97117324e382c20999adaf1d190483
jmaliksi <joe.maliksi@gmail.com>
2016-04-26 10:00:34 -0700

    Page crawling

--- README.rst
+++ README.rst
@@ -1,8 +1,10 @@
 Installation
-# virtualenv env
-# source env/bin/activate
-# pip install -r requirements.txt
+----
+* virtualenv env
+* source env/bin/activate
+* pip install -r requirements.txt
 
 Usage
-# cd aesimplifier
-# scrapy crawl exy
+----
+* cd aesimplifier
+* scrapy crawl exy
--- aesimplifier/aesimplifier/spiders/exy.py
+++ aesimplifier/aesimplifier/spiders/exy.py
@@ -7,14 +7,23 @@ class Exy(scrapy.Spider):
     name = "exy"
     allowed_domains = ["archipelagoexodus.proboards.com"]
     start_urls = [
-        "http://archipelagoexodus.proboards.com/thread/3503/dis-orientation",
-        "http://archipelagoexodus.proboards.com/thread/3503/dis-orientation?page=2",
-        "http://archipelagoexodus.proboards.com/thread/3503/dis-orientation?page=3",
-        "http://archipelagoexodus.proboards.com/thread/3503/dis-orientation?page=4",
-        "http://archipelagoexodus.proboards.com/thread/3503/dis-orientation?page=5",
+        "http://archipelagoexodus.proboards.com/thread/3503/dis-orientation?page=1",
+        # "http://archipelagoexodus.proboards.com/thread/3503/dis-orientation?page=2",
+        # "http://archipelagoexodus.proboards.com/thread/3503/dis-orientation?page=3",
+        # "http://archipelagoexodus.proboards.com/thread/3503/dis-orientation?page=4",
+        # "http://archipelagoexodus.proboards.com/thread/3503/dis-orientation?page=5",
     ]
 
     def parse(self, response):
+        links = response.selector.xpath('//a[re:test(@href, "/thread/\\d*/.*\\?page=\\d+")]/@href').extract()
+        for link in links:
+            # remove this when we go to full crawling
+            if link[:link.index('?')] not in response.url:
+                continue
+            url = response.urljoin(link)
+            yield scrapy.Request(url, callback=self.parse_page)
+
+    def parse_page(self, response):
         title = response.selector.xpath('//h1').extract()[0]
         posts = response.selector.xpath('//tr[contains(@class, "post")]')
         for post in posts:
@@ -24,11 +33,3 @@ class Exy(scrapy.Spider):
             p['post_id'] = post.xpath('@id').extract()[0]
             p['title'] = title
             yield p
-
-        '''
-        # go to next page if it exists
-        page_match = re.match(r'(.*)\?page=(\d+)', response.url)
-        url = page_match.group(1) if page_match else response.url
-        page = int(page_match.group(2)) if page_match else 1
-        yield scrapy.Request('{}?page={}'.format(url, page + 1), callback=self.parse)
-        '''
